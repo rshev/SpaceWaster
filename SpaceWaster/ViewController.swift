ac4f795918770df64ffe7a09156757f22468766b
Roman Shevtsov <roman.shevtsov@gmail.com>
2017-11-11 19:49:08 +0000

    Initial Commit

---
+++ SpaceWaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceWaster
+//
+//  Created by asdfgh1 on 11/11/2017.
+//  Copyright © 2017 Roman Shevtsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
